76b4e7ce8c24f35cbce511e96e998fb36e33932a
jonschr <jonschr@gmail.com>
2020-09-04 21:26:21 -0500

    Bugfix for themes that don't use colors

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,6 @@
+### 1.0.3
+* BUGFIX: bail on getting the colors if the theme doesn't declare any
+
 ### 1.0.1
 * Updating how we load fields to remove the json sync (seems easy to run into conflicts), just loading from php instead.
 * Minor fixes to a few fields with bad defaults.
--- elodin-section-block.php
+++ elodin-section-block.php
@@ -3,7 +3,7 @@
 	Plugin Name: Elodin Block: Sections
 	Plugin URI: https://github.com/jonschr/elodin-section-block
     Description: Just another section block
-	Version: 1.0.2
+	Version: 1.0.3
     Author: Jon Schroeder
     Author URI: https://elod.in
 
@@ -28,7 +28,7 @@ if ( !defined( 'ABSPATH' ) ) {
 define( 'ELODIN_SECTION_BLOCK', dirname( __FILE__ ) );
 
 // Define the version of the plugin
-define ( 'ELODIN_SECTION_BLOCK_VERSION', '1.0.2' );
+define ( 'ELODIN_SECTION_BLOCK_VERSION', '1.0.3' );
 
 require_once( 'acf-json/fields.php' );
 
@@ -115,11 +115,13 @@ function elodin_section_block_render( $block, $content = '', $is_preview = false
     
     //* color matching for background colors
     $colors = current( (array) get_theme_support( 'editor-color-palette' ) );
-    foreach( $colors as $color ) {
-        if ( in_array( $background_color, $color ) ) {
-            $className .= ' has-' . $color['slug'] . '-background-color';
-            $background_color = null;
-        }            
+    if ( $colors ) {
+        foreach( $colors as $color ) {
+            if ( in_array( $background_color, $color ) ) {
+                $className .= ' has-' . $color['slug'] . '-background-color';
+                $background_color = null;
+            }            
+        }
     }
             
     //* Render
