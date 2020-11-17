1618d6f8fd108d62bbb6ab2f12bc51da0a2952ba
jonschr <jonschr@gmail.com>
2020-11-17 15:59:00 -0600

    1.0.7

--- elodin-section-block.php
+++ elodin-section-block.php
@@ -3,7 +3,7 @@
 	Plugin Name: Elodin Block: Sections
 	Plugin URI: https://github.com/jonschr/elodin-section-block
     Description: Just another section block
-	Version: 1.0.6
+	Version: 1.0.7
     Author: Jon Schroeder
     Author URI: https://elod.in
 
@@ -27,7 +27,7 @@ if ( !defined( 'ABSPATH' ) ) {
 define( 'ELODIN_SECTION_BLOCK', dirname( __FILE__ ) );
 
 // Define the version of the plugin
-define ( 'ELODIN_SECTION_BLOCK_VERSION', '1.0.6' );
+define ( 'ELODIN_SECTION_BLOCK_VERSION', '1.0.7' );
 
 require_once( 'acf-json/fields.php' );
 
